6880da2bd3cbdd3a2476e1c975d248973e86ee0b
Tyler Krys <tylerkrys@gmail.com>
2017-08-10 17:35:49 +0000

    Refactors question.rb to be a class within module MathGame

--- question.rb
+++ question.rb
@@ -1,25 +1,26 @@
-class Question
+# question.rb
 
-  attr_reader :question, :answer
+module MathGame
+  class Question
 
-  # Initalize a new question with nil for question and answer
-  def initialize
-    @question = nil
-    @answer = nil
+    attr_reader :answer, :correct
+
+    # Create two random numbers and set default answer status to incorrect
+    def initialize
+      @number1 = rand(1..20)
+      @number2 = rand(1..20)
+      @correct = false
+      ask
+    end
+
+    # Ask the player a question and get their response
+    def ask
+      puts "What is the sum of #{@number1} and #{@number2}?"
+      response = gets.chomp.to_i
+      if response == @number1 + @number2
+        @correct = true
+      end
+    end
+
   end
-
-  # Come up with two pseudo-random numbers between 1-2
-  def get_numbers
-    number1 = rand(1..20)
-    number2 = rand(1..20)
-  end
-
-  def get_answer
-    @answer = number1 + number2
-  end
-
-  def get_question
-    @question = "What is the sum of #{number1} and #{number2} ?"
-  end
-
 end
