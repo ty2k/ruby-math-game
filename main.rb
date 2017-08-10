c93403a97e8d94560509cd2104ee769c7bf73295
Tyler Krys <tylerkrys@gmail.com>
2017-08-10 17:33:47 +0000

    Adds initial game instructions to main.rb, adds requirement for game.rb

--- main.rb
+++ main.rb
@@ -1,7 +1,18 @@
 # main.rb
 
-require './player'
-require './question'
-require './turn'
+require './game.rb'
+require './player.rb'
+require './question.rb'
+require './turn.rb'
 
-puts "Welcome to the Math Game"
+puts "\n
+  +-----------------------------------------------------------------+
+  |  Welcome to the Ruby Math Game. This is a two-player game.      |
+  |  After selecting names for each player, the game will start.    |
+  |  Each player takes turns answer a skill-testing math question.  |
+  |  If a player answers incorrectly, they lose a life.             |
+  |  Each player starts with three lives.                           |
+  |  The game ends when one player loses all of their lives.        |
+  +-----------------------------------------------------------------+\n\n\n"
+
+MathGame::Game.new
